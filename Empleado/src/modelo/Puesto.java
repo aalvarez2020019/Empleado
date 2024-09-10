67c5a0df2f62b255c2022782862465a65d733da2
Axel Alvarez <axjavier4alvarez44@gmail.com>
2024-09-09 22:25:57 -0600

    cambios

---
+++ Empleado/src/modelo/Puesto.java
@@ -0,0 +1,70 @@
+/*@author balvarez
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelo;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+import java.sql.ResultSet;
+import java.util.HashMap;
+import javax.swing.DefaultComboBoxModel;
+
+
+/**
+ *
+ * @author Bomiki
+ */
+public class Puesto{
+    private int id_po;
+    private String puestos;
+    Conexion cn;
+    
+    public Puesto(){}
+
+    public Puesto(int id_po, String puesto) {
+       this.id_po = id_po;
+       this.puestos = puesto;
+    }
+
+    public int getId_po() {
+        return id_po;
+    }
+
+    public void setId_po(int id_po) {
+        this.id_po = id_po;
+    }
+
+    public String getPuesto() {
+        return puestos;
+    }
+
+    public void setPuesto(String puesto) {
+        this.puestos = puesto;
+    }
+
+public HashMap<String, Integer> map = new HashMap<>();
+
+public DefaultComboBoxModel<String> leer() {
+    DefaultComboBoxModel<String> p = new DefaultComboBoxModel<>();
+    try {
+        cn = new Conexion();
+        cn.abrir_conexion();
+        String query = "select * from puestos;";
+        ResultSet consulta = cn.conexionDB.createStatement().executeQuery(query);
+        while (consulta.next()) {
+            int id = consulta.getInt("id_puesto");
+            String puesto = consulta.getString("puesto");
+           p.addElement(puesto);
+            map.put(puesto,id);
+        }
+        cn.cerrar_conexion();
+    } catch (SQLException ex) {
+        cn.cerrar_conexion();
+        JOptionPane.showMessageDialog(null, "Error al leer los datos: " + ex.getMessage());
+    }
+    return p;
+}
+
+}
+
+   
---
+++ base de datos empresa.sql
@@ -0,0 +1,124 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: db_empresa
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clientes`
+--
+
+drop database if exists db_empresa;
+
+create database db_empresa;
+use db_empresa;
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `id_clientes` int NOT NULL AUTO_INCREMENT,
+  `nit` varchar(9) DEFAULT NULL,
+  `nombres` varchar(60) DEFAULT NULL,
+  `apellidos` varchar(60) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` varchar(8) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  PRIMARY KEY (`id_clientes`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+INSERT INTO `clientes` VALUES (1,'1236264','Brandon','Kloan','Guatemala','87451232','1998-10-10'),(3,'10552623','Arnoldo','Patricio','Guatemala','55662233','1999-10-15'),(4,'51516','Gimena','Herrera','Mexico','51513265','1999-10-15'),(5,'5615156','Jaqueline','Matriaz','Mexico','1651','2002-05-15'),(6,'5615156','Jaqueline','Matriaz','Mexico','1651','2002-05-15');
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `empleados`
+--
+
+DROP TABLE IF EXISTS `empleados`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `empleados` (
+  `id_empleados` int NOT NULL AUTO_INCREMENT,
+  `codigo` varchar(20) DEFAULT NULL,
+  `nombres` varchar(60) DEFAULT NULL,
+  `apellidos` varchar(60) DEFAULT NULL,
+  `direccion` varchar(100) DEFAULT NULL,
+  `telefono` varchar(8) DEFAULT NULL,
+  `fecha_nacimiento` date DEFAULT NULL,
+  `id_puesto` smallint DEFAULT NULL,
+  PRIMARY KEY (`id_empleados`),
+  KEY `id_puesto_puesto_empleados_idx` (`id_puesto`),
+  CONSTRAINT `id_puesto_puesto_empleados` FOREIGN KEY (`id_puesto`) REFERENCES `puestos` (`id_puesto`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `empleados`
+--
+
+LOCK TABLES `empleados` WRITE;
+/*!40000 ALTER TABLE `empleados` DISABLE KEYS */;
+INSERT INTO `empleados` VALUES (1,'E001','Julio','Perez','Ciudad','15261425','1999-05-08',1),(2,'E002','Maria','Herrera','Ciudad','23326565','2000-10-09',2),(3,'E003','Marta','Himbrea','Ciudad','21653474','1998-05-10',3),(4,'E004','Lalo','Sorana','Guatemala','12235645','1995-09-09',5),(6,'E005','Wilmer','Solona','Guatemala','23565421','1995-12-20',7);
+/*!40000 ALTER TABLE `empleados` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `puestos`
+--
+
+DROP TABLE IF EXISTS `puestos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `puestos` (
+  `id_puesto` smallint NOT NULL AUTO_INCREMENT,
+  `puesto` varchar(40) DEFAULT NULL,
+  PRIMARY KEY (`id_puesto`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `puestos`
+--
+
+LOCK TABLES `puestos` WRITE;
+/*!40000 ALTER TABLE `puestos` DISABLE KEYS */;
+INSERT INTO `puestos` VALUES (1,'Contabilidad'),(2,'Programador'),(3,'Gerente de IT'),(4,'Financiero'),(5,'Contador general'),(6,'Servicio de limpieza'),(7,'Subcontrato'),(8,'Guardia de seguridad'),(9,'Bodeguero'),(10,'Auxiliar de compras'),(11,'Encargado de compras');
+/*!40000 ALTER TABLE `puestos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'db_empresa'
+--
+
+--
+-- Dumping routines for database 'db_empresa'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-01 23:23:45
